79e175b4b58ff30222e2f33de98efbeec7addb2a
ssudhakar4799 <104546063+ssudhakar4799@users.noreply.github.com>
2022-05-25 21:08:21 +0530

    Add files via upload

---
+++ project-(1)-3/calculator.js
@@ -0,0 +1,115 @@
+
+//input box
+var number=document.getElementById("number")
+
+
+var button1=document.getElementById('btn1');
+button1.addEventListener('click',one);
+console.log(button1);
+
+function one() {
+   var one=1;
+   console.log(one);
+   number.value+=one 
+   
+}
+var button2=document.getElementById('btn2');
+button2.addEventListener('click',two);
+console.log(button2);
+function two() {
+   var two=2;
+   console.log(two);
+   number.value+=two   
+}
+var button3=document.getElementById('btn3');
+button3.addEventListener('click',three);
+console.log(button3);
+function three() {
+   var three=3;
+   console.log(three);
+   number.value+=three
+    
+}
+var button4=document.getElementById('btn4');
+button4.addEventListener('click',four);
+console.log(button4);
+function four() {
+   var four=4;
+   console.log(four);
+   number.value+=four
+    
+}
+var button5=document.getElementById('btn5');
+button5.addEventListener('click',five);
+console.log(button5);
+function five() {
+   var five=5;
+   console.log(five);
+   number.value+=five
+    
+}
+var button6=document.getElementById('btn6');
+button6.addEventListener('click',six);
+console.log(button6);
+function six() {
+   var six=6;
+   console.log(six);
+   number.value+=six
+    
+}
+var button7=document.getElementById('btn7');
+button7.addEventListener('click',seven);
+console.log(button7);
+function seven() {
+   var seven=7;
+   console.log(seven);
+   number.value+=seven
+}
+var button8=document.getElementById('btn8');
+button8.addEventListener('click',eight);
+console.log(button8);
+function eight() {
+   var eight=8;
+   console.log(eight);
+   number.value+=eight
+    
+}
+var button9=document.getElementById('btn9');
+button9.addEventListener('click',nine);
+console.log(button9);
+function nine() {
+   var nine=9;
+   console.log(nine);
+   number.value+=nine
+    
+}
+var button10=document.getElementById('btn0');
+button10.addEventListener('click',zero);
+console.log(button10);
+function zero() {
+   var zero=0;
+   console.log(zero);
+   number.value+=zero
+    
+}
+var button11=document.getElementById('btnr');
+button11.addEventListener('click',clear);
+console.log(button11);
+function clear() {
+    var clear =null;
+    console.log(clear);
+    number.value+=clear
+}
+
+
+// var num=document.getElementById('number')
+// console.log(num);
+
+
+
+// var buttons=document.getElementById('btn1')
+// buttons.addEventListener('click',add)
+// function add(){
+   
+// }
+
